a339ffdbe3c2d23d221d071c337325f34993fea5
AxelColliaux <axel.colliaux@hotmail.fr>
2023-02-27 12:43:22 +0100

    challenge ok?

---
+++ Decipherer.java
@@ -0,0 +1,16 @@
+class Decipherer {
+    public static void main(String[] args){
+        System.out.println(decipher("0@sn9sirppa@#?ia'jgtvryko1")); 
+        System.out.println(decipher("q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj"));
+        System.out.println(decipher("aopi?sedohtém@#?sedhtmg+p9l!"));
+    }
+
+    public static String decipher(String string){
+        int keyNumber = (string.length())/2;
+        String substring = string.substring(6, keyNumber);
+        substring = substring.replace('@', ' ');
+        substring = substring.replace('#', ' ');
+        substring = substring.replace('?', ' ');
+        return new StringBuilder(substring).reverse().toString();
+    }
+}
